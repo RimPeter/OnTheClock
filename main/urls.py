f622a7cbaac3eb4fd963a9ad7ca91a63bf7c226b
RimPeter <hull@localappliancerentals.co.uk>
2024-10-06 09:33:50 +0100

    Add NiceAdmin htmls
    https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
+    path('chart-apexcharts/', views.chartapexcharts, name='chart-apexcharts'),
+    path('charts-chartjs/', views.chartschartjs, name='charts-chartjs'),
+    path('charts-echarts/', views.chartsecharts, name='charts-echarts'),
+    path('components-accordions/', views.componentsaccordions, name='components-accordions'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def main(request):
-    return render(request, 'main/index.html')
+    return render(request, 'main/main.html')
+
+def chartapexcharts(request):
+    return render(request, 'main/chart-apexcharts.html')
+
+def chartschartjs(request):
+    return render(request, 'main/charts-chartjs.html')
+
+def chartsecharts(request):
+    return render(request, 'main/charts-echarts.html')
+
+def componentsaccordions(request):
+    return render(request, 'main/components-accordions.html')
