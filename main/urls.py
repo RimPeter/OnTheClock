2a67ab8253001ba0a41a99a1c4ac802424142a42
RimPeter <hull@localappliancerentals.co.uk>
2024-10-06 21:46:51 +0100

    Replaceing blocks in NiceAdmin templates

--- main/urls.py
+++ main/urls.py
@@ -3,8 +3,48 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
+    
     path('chart-apexcharts/', views.chartapexcharts, name='chart-apexcharts'),
+    
     path('charts-chartjs/', views.chartschartjs, name='charts-chartjs'),
+    
     path('charts-echarts/', views.chartsecharts, name='charts-echarts'),
+    
     path('components-accordions/', views.componentsaccordions, name='components-accordions'),
+    
+    path('components-alerts/', views.componentsalerts, name='components-alerts'),
+    
+    path('components-badges/', views.componentsbadges, name='components-badges'),
+    path('components-breadcrumbs/', views.componentsbreadcrumbs, name='components-breadcrumbs'),
+    path('components-buttons/', views.componentsbuttons, name='components-buttons'),
+    path('components-cards/', views.componentscards, name='components-cards'),
+    path('components-carousel/', views.componentscarousel, name='components-carousel'),
+    path('components-list-group/', views.componenntslistgroup, name='components-list-group'),
+    path('components-modals/', views.componentsmodals, name='components-modals'),
+    path('components-pagination/', views.componentspagination, name='components-pagination'),
+    path('components-progress/', views.componentsprogress, name='components-progress'),
+    path('components-spinners/', views.componentsspinners, name='components-spinners'),
+    path('components-tabs/', views.componentstabs, name='components-tabs'),
+    path('components-tooltips/', views.componentstooltips, name='components-tooltips'),
+    path('forms-editors/', views.formseditors, name='forms-editors'),
+    path('forms-elements/', views.formselements, name='forms-elements'),
+    path('forms-layouts/', views.formslayouts, name='forms-layouts'),
+    path('forms-validation/', views.formsvalidation, name='forms-validation'),
+    path('icons-bootstrap/', views.iconsbootstrap, name='icons-bootstrap'),
+    path('icons-boxicons/', views.iconsboxicons, name='icons-boxicons'),
+    path('icons-remix/', views.iconsremix, name='icons-remix'),
+    path('pages-blank/', views.pagesblank, name='pages-blank'),
+    path('pages-contact/', views.pagescontact, name='pages-contact'),
+    path('pages-error-404/', views.pageserror404, name='pages-error-404'),
+    path('pages-faq/', views.pagesfaq, name='pages-faq'),
+    path('pages-login', views.pageslogin, name='pages-login'),
+    path('pages-register/', views.pagesregister, name='pages-register'),
+    path('tables-data/', views.tablesdata, name='tables-data'),
+    
+    path('tables-general/', views.tablesgeneral, name='tables-general'),
+    
+    path('users-profile/', views.usersprofile, name='users-profile'),
+    
+    
+    
 ]
--- main/views.py
+++ main/views.py
@@ -5,7 +5,7 @@ def main(request):
     return render(request, 'main/main.html')
 
 def chartapexcharts(request):
-    return render(request, 'main/chart-apexcharts.html')
+    return render(request, 'main/charts-apexcharts.html')
 
 def chartschartjs(request):
     return render(request, 'main/charts-chartjs.html')
@@ -15,3 +15,90 @@ def chartsecharts(request):
 
 def componentsaccordions(request):
     return render(request, 'main/components-accordions.html')
+
+def componentsalerts(request):
+    return render(request, 'main/components-alerts.html')
+
+def componentsbadges(request):
+    return render(request, 'main/components-badges.html')
+
+def componentsbreadcrumbs(request):
+    return render(request, 'main/components-breadcrumbs.html')
+
+def componentsbuttons(request):
+    return render(request, 'main/components-buttons.html')
+
+def componentscards(request):
+    return render(request, 'main/components-cards.html')
+
+def componentscarousel(request):
+    return render(request, 'main/components-carousel.html')
+
+def componenntslistgroup(request):
+    return render(request, 'main/components-list-group.html')
+
+def componentsmodals(request):
+    return render(request, 'main/components-modals.html')
+
+def componentspagination(request):
+    return render(request, 'main/components-pagination.html')
+
+def componentsprogress(request):
+    return render(request, 'main/components-progress.html')
+
+def componentsspinners(request):
+    return render(request, 'main/components-spinners.html')
+
+def componentstabs(request):
+    return render(request, 'main/components-tabs.html')
+
+def componentstooltips(request):
+    return render(request, 'main/components-tooltips.html')
+
+def formseditors(request):
+    return render(request, 'main/forms-editors.html')
+
+def formselements(request):
+    return render(request, 'main/forms-elements.html')
+
+def formslayouts(request):
+    return render(request, 'main/forms-layouts.html')
+
+def formsvalidation(request):
+    return render(request, 'main/forms-validation.html')
+
+def iconsbootstrap(request):
+    return render(request, 'main/icons-bootstrap.html')
+
+def iconsboxicons(request):
+    return render(request, 'main/icons-boxicons.html')
+
+def iconsremix(request):
+    return render(request, 'main/icons-remix.html')
+
+def pagesblank(request):
+    return render(request, 'main/pages-blank.html')
+
+def pagescontact(request):
+    return render(request, 'main/pages-contact.html')
+
+def pageserror404(request):
+    return render(request, 'main/pages-error-404.html')
+
+def pagesfaq(request):
+    return render(request, 'main/pages-faq.html')
+
+def pageslogin(request):
+    return render(request, 'main/pages-login.html')
+
+def pagesregister(request):
+    return render(request, 'main/pages-register.html')
+
+def tablesdata(request):
+    return render(request, 'main/tables-data.html')
+
+def tablesgeneral(request):
+    return render(request, 'main/tables-general.html')
+
+def usersprofile(request):
+    return render(request, 'main/users-profile.html')
